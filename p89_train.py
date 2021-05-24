601869fd70e65a178f47f24e976ae61cd798b5d4
Harshkaithwas <53550463+Harshkaithwas@users.noreply.github.com>
2021-05-24 23:20:39 +0530

    Add files via upload

---
+++ p89_train.py
@@ -0,0 +1,31 @@
+class train:
+      def __init__(self, name, fare, seats):
+            self.name = name
+            self.fare = fare
+            self.seats = seats
+      
+      def getStatus(self):
+            print(f"The name of the train is {self.name}")
+            print(f"the seats available in the train are {self.seats}")
+
+      def fareInfo(self):
+            print(f"the fare of the train for this journey {self.fare}")
+
+      def bookTicket(self):
+            if(self.seats>0):
+                  print(f"Your ticket has been booked ! Your seat number is {self.seats}")
+                  self.seats = self.seats - 1
+            else:
+                  print("bookings are full thanks")
+
+      def cancleTicket(self, seat):#pending
+            
+            pass
+
+
+interCity = train("InterCity Exoress :  140123", 90, 2)
+interCity.getStatus()
+interCity.fareInfo()
+interCity.bookTicket()
+interCity.bookTicket()
+interCity.getStatus()
