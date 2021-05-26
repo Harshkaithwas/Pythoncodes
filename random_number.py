7fd291fed79838cf7658f1968d4292d0bc556d15
Harshkaithwas <53550463+Harshkaithwas@users.noreply.github.com>
2021-05-27 03:51:26 +0530

    Update random_number.py

--- random_number.py
+++ random_number.py
@@ -1,11 +1,12 @@
-
+#fun game to guess a random number
 import random
-randNumber = random.randint(1, 5)
-print(randNumber)
+randNumber = random.randint(1, 10)
+# print(randNumber)
 userGuess = None
 guesses = 0
 while(userGuess != randNumber):    
       userGuess = int(input("Enter yout guess"))
+      guesses +=1
 
       if (userGuess == randNumber):
             print("Entry matched")
@@ -17,3 +18,11 @@ while(userGuess != randNumber):
                   
 
 print(f"Your guess the number in {guesses} guesses")
+with open("hiscore.txt","r") as f:
+      hiscore = int(f.read())
+
+if(guesses < hiscore):
+      print("its a new highscore")
+      with open("hiscore.txt","w") as f:
+            f.write(str(guesses))
+      
